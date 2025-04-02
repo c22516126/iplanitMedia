a19b01cdb58601330289ddb74cdede4e7eb4eeca
Jason Garcia <c22516126@mytudublin.ie>
2025-04-02 02:38:49 +0100

    react should be working now

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Media from './media'; // Make sure the file is named Media.jsx and lives in src/
+
+function App() {
+  return (
+    <div className="App">
+      <Media />
+    </div>
+  );
+}
+
+export default App;
